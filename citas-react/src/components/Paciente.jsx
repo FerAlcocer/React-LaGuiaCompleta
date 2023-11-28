2276cf885133010df07c28b8afb8a2048691fbf0
Fernando Alcocer <fernandoalcocer812@hotmail.com>
2023-11-28 19:59:12 -0300

    Avances en el proyecto #1 Citas

---
+++ citas-react/src/components/Paciente.jsx
@@ -0,0 +1,33 @@
+const Paciente = ({ paciente }) => {
+  console.log(paciente);
+  return (
+    <div className="mx-5 my-5 bg-white shadow-md px-5 py-10 rounded-md">
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Nombre: {""}
+        <span className="font-normal normal-case ">{paciente.nombre}</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Propietario: {""}
+        <span className="font-normal normal-case ">{paciente.propietario}</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Email: {""}
+        <span className="font-normal normal-case ">{paciente.email}</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Fecha Alta: {""}
+        <span className="font-normal normal-case ">{paciente.fecha}</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Sintomas: {""}
+        <span className="font-normal normal-case ">{paciente.sintomas}</span>
+      </p>
+    </div>
+  );
+};
+
+export default Paciente;
